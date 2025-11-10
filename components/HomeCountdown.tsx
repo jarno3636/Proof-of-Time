fc3de2b3f49268997ec5bc57d206707c8b94ad1c
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-11-10 16:43:21 -0600

    Delete components/HomeCountdown.tsx

--- components/HomeCountdown.tsx
+++
@@ -1,8 +0,0 @@
-// components/HomeCountdown.tsx
-"use client";
-import Countdown from "./Countdown";
-export default function HomeCountdown() {
-  const end = Number(process.env.NEXT_PUBLIC_PRESALE_END_UNIX || 0);
-  if (!end) return null;
-  return <Countdown target={end} />;
-}
